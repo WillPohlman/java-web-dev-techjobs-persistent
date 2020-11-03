525d5e2f0ce0a4647200f0b89adfd29636a3b7a8
WillPohlman <william.a.pohlman@gmail.com>
2020-11-02 19:09:45 -0600

    Completed Part 2

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,39 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 1, max = 50, message = "Location must be between 1 and 50 characters")
+    private String location;
 
+    @OneToMany
+    @JoinColumn
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Employer(@NotBlank @Size(min = 1, max = 100, message = "Name must be between 1 and 100 characters") String name, @NotBlank @Size(min = 1, max = 50, message = "Location must be between 1 and 50 characters") String location)
+    {
+        this.setName(name);
+        this.location = location;
+    }
+
+    public Employer(){}
+
+    public String getLocation()
+    {
+        return location;
+    }
+
+    public void setLocation(String location)
+    {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,31 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 1, max = 250, message = "Description must be between 1 and 250 characters")
+    private String description;
+
+    public Skill(@NotBlank @Size(min = 1, max = 100, message = "Name must be between 1 and 100 characters") String name, @NotBlank @Size(min = 1, max = 250, message = "Description must be between 1 and 250 characters") String description)
+    {
+        this.setName(name);
+        this.description = description;
+    }
+
+    public Skill(){}
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
 }
